c6caa9cb1863e49d8e9c5615b0e09ac5fbebbea8
wenhaoran <262856298@qq.com>
2016-09-28 11:13:23 +0800

    增加controller

---
+++ Mamba-integration/Mamba-webapp/src/main/java/com/mamba/usm/web/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.mamba.usm.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class LoginController {
+	@RequestMapping("/login")
+	public void login() {
+		
+	}
+}
